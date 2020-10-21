abbf68f68d4fface32230279e1a95b34f329c61e
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-10-21 20:46:41 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     const paragraph="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
     <div id="main">
-      // Do not alter the main div
+  
     <button id="click">
-      <p id="para">paragraph</p>
+      <p id="para">{paragraph}</p>
       </button>
     </div>
   );
