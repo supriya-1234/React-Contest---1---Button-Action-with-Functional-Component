955548192d7f1f56925d28979f903438b0d4c9ae
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-10-22 20:44:52 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,21 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [para,setPara] =React.useState(false);
-  function renderPara(){
-    return(<p id="para">"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"</p>);
+  const [para, setPara] = React.useState(false);
+  function renderPara() {
+    return (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    );
   }
   return (
     <div id="main">
-      <button id="click" onClick={()=>{setPara(true)}>
-       Click 
+      <button id="click" onClick={() => setPara(true)}>
+        Click
       </button>
-      {para?renderPara():""}
+      {para ? renderPara() : ""}
     </div>
   );
 }
